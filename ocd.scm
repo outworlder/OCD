f861ae09964840e17fdbf4ceef96a8ff875584c5
Stephen Eilert <spedrosa@gmail.com>
2011-11-29 20:06:50 -0300

    Created a function to list files. It has a bug.
    Sometimes, it returns an empty list. Still need to figure out when it happens. Otherwise, it works fine, recursively grabbing filenames and timestamps.

---
+++ ocd.scm
@@ -0,0 +1,24 @@
+;;; OCD - Egg for running tests repeatedly.
+
+;;; Get the file listing
+
+(use files)                             ; Files and pathname operations
+(use posix)
+
+(define ocd-root-directory (make-parameter "."))
+
+(define (compile-files-list path)
+  (print "Entering directory" path)
+  (if (directory? path)  ;; Stop condition 
+      (let ([listing (directory path)])
+        (map (lambda (d)
+               (let ([absolute-path (normalize-pathname (make-absolute-pathname path d))])
+                 (if (directory? absolute-path)
+                     (compile-files-list absolute-path)
+                     (list absolute-path (file-modification-time absolute-path))))) listing))
+      path))
+
+;;; Walk the trees and return files that have been modified (
+(define (get-modified before after)
+  #f)
+
