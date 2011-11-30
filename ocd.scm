c1f124d0e848e11804f1c07a938dd7f9df1a6595
Stephen Eilert <spedrosa@gmail.com>
2011-11-30 19:09:01 -0300

    Replaced the implementation with a hash-based and full of side effects.

--- ocd.scm
+++ ocd.scm
@@ -4,25 +4,31 @@
 
 (use files)                             ; Files and pathname operations
 (use posix)
+(use srfi-69)                           ;Hash tables
 
 (define ocd-root-directory (make-parameter (current-directory)))
 
 (define (compile-files-list path)
+  (let ([ht (make-hash-table string= string-hash 100)])
+    (walk-directories! ht path)
+    ht))
+
+(define (walk-directories! ht path)
   ;(print "Entering directory" path)
   (if (directory? path)  ;; Stop condition 
       (let ([listing (directory path)])
-        (map (lambda (d)
-               (handle-exceptions exn (begin
-                                        (print exn)
-                                        '()) 
-                                  (let ([absolute-path (normalize-pathname (make-absolute-pathname path d))])
-                                    (if (directory? absolute-path)
-                                        (compile-files-list absolute-path)
-                                        (list absolute-path (file-modification-time absolute-path)))))) listing))
+        (for-each (lambda (d)
+                    (handle-exceptions exn (begin
+                                             (print exn)
+                                             '()) 
+                                       (let ([absolute-path (normalize-pathname (make-absolute-pathname path d))])
+                                         (if (directory? absolute-path)
+                                             (walk-directories ht absolute-path)
+                                             (hash-table-set! ht absolute-path (file-modification-time absolute-path)))))) listing))
       path))
 
 ;;; Walk the trees and return files that have been modified.
 (define (get-modified before after)
   #f)
 
-(compile-files-list (current-directory))
+(print (hash-table->alist (compile-files-list (current-directory))))
