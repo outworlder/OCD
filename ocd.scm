7993cf3501a8798b99d212dbd2c22e9f4c44f592
Stephen Eilert <spedrosa@gmail.com>
2011-11-30 16:49:44 -0300

    Walking the tree now works. Added a workaround for a "stat" Chicken bug. See below.
    Chicken Scheme uses stat to gather file information. On 32 bit platforms, if the standard stat function is used and the file is greater than 2gb, an EOVERFLOW will be thrown. It is unclear how to solve this with Chicken (except by compiling with -D_FILE_OFFSET_BITS=64)

--- ocd.scm
+++ ocd.scm
@@ -5,20 +5,24 @@
 (use files)                             ; Files and pathname operations
 (use posix)
 
-(define ocd-root-directory (make-parameter "."))
+(define ocd-root-directory (make-parameter (current-directory)))
 
 (define (compile-files-list path)
-  (print "Entering directory" path)
+  ;(print "Entering directory" path)
   (if (directory? path)  ;; Stop condition 
       (let ([listing (directory path)])
         (map (lambda (d)
-               (let ([absolute-path (normalize-pathname (make-absolute-pathname path d))])
-                 (if (directory? absolute-path)
-                     (compile-files-list absolute-path)
-                     (list absolute-path (file-modification-time absolute-path))))) listing))
+               (handle-exceptions exn (begin
+                                        (print exn)
+                                        '()) 
+                                  (let ([absolute-path (normalize-pathname (make-absolute-pathname path d))])
+                                    (if (directory? absolute-path)
+                                        (compile-files-list absolute-path)
+                                        (list absolute-path (file-modification-time absolute-path)))))) listing))
       path))
 
-;;; Walk the trees and return files that have been modified (
+;;; Walk the trees and return files that have been modified.
 (define (get-modified before after)
   #f)
 
+(compile-files-list (current-directory))
